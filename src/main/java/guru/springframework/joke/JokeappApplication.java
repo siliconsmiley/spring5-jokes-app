0fc62e94ce24e6f7cfdd0c58b3f1e4df75aa9c5f
Matthew Wagner <siliconsmiley@gmail.com>
2020-05-14 11:38:50 -0400

    Added javadoc.

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -3,6 +3,9 @@ package guru.springframework.joke;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Simple Chuck Norris joke generator.
+ */
 @SpringBootApplication
 public class JokeappApplication {
 
