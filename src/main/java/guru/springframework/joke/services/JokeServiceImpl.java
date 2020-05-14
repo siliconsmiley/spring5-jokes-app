61957576d0447e053bcb8db8c55d1c858ac32498
Matthew Wagner <siliconsmiley@gmail.com>
2020-05-14 12:42:36 -0400

    Created joke controller

--- src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.joke.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
 
 /**
  * Get a quote from the random generator.
  */
+@Service
 public class JokeServiceImpl implements JokeService {
     private final ChuckNorrisQuotes quotes;
 
