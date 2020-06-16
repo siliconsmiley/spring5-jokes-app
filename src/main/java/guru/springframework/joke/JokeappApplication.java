472fd745d689bf054077268efee3385393423c4d
Matthew Wagner <siliconsmiley@gmail.com>
2020-06-16 09:42:00 -0400

    Refactored to use Spring XML configuration.

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -2,11 +2,13 @@ package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Simple Chuck Norris joke generator.
  */
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
 
 	public static void main(String[] args) {
